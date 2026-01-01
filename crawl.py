e86720153e4de0ba471bf0d4b8601c95ecee7e2d
DNS <37093+DNS@users.noreply.github.com>
2026-01-01 19:42:21 +0700

    switched to Selenium

---
+++ crawl.py
@@ -0,0 +1,50 @@
+# pip install selenium
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import json, sys, html2text
+
+options = webdriver.FirefoxOptions()
+options.add_argument("-headless")
+
+# make sure you have no other instance of firefox running that uses this profile or it will throw an error without a helpful message
+#options.add_argument('-profile')
+#options.add_argument('/home/dns/.mozilla/firefox/dfpyhtbw.DNS')
+#options.profile = '/home/dns/.mozilla/firefox/dfpyhtbw.DNS'
+
+#options.set_preference('devtools.jsonview.enabled', False)
+'''
+# rest
+fp = webdriver.FirefoxProfile('/home/dns/.mozilla/firefox/dfpyhtbw.DNS')
+options.FirefoxProfile = fp
+driver = webdriver.Firefox(options=options)
+#driver.get(f'{sys.argv[1]}')
+driver.get(f'view-source:https://www.idx.co.id/primary/helper/GetStockChart?indexCode=BUMI&period=1Y')
+content = driver.find_element(By.TAG_NAME, 'pre').text
+#content = driver.find_element(By.XPATH, "//div[@id='json']").text
+parsed_json = json.loads(content)
+print(parsed_json)
+'''
+
+
+#fp = webdriver.FirefoxProfile('/home/dns/.mozilla/firefox/dfpyhtbw.DNS')
+#options.FirefoxProfile = fp
+driver = webdriver.Firefox(options=options)
+#driver.get(f'{sys.argv[1]}')
+driver.get('https://id.jobstreet.com/jobs-in-information-communication-technology/remote')
+content = driver.page_source
+
+h = html2text.HTML2Text()
+h.wrap_links = False
+txt = h.handle(content)
+print(txt)
+
+
+
+
+driver.close()
+driver.quit()
+
+
+
+
--- jobstreet.ps1
+++ jobstreet.ps1
@@ -1,77 +1,27 @@
-﻿# jobstreet.ps1 — Search jobs with specific keyword
+#$a = py ./crawl.py
 
-if ($args) {
-	$bulk_url = $args
-} else {
-	$bulk_url = $input | % { $_ }
-}
+$base_url = 'https://id.jobstreet.com'
 
-if (-not $bulk_url) {
-	Write-Output 'jobstreet.ps1 — Search jobs with specific keyword'
-	Write-Output 'Usage:'
-	Write-Output '	jobstreet [URL1] [URL2]'
-	Write-Output "	'[URL1]', '[URL2]', '[URL3]' | jobstreet"
-	exit
-}
+$a = gc test.txt -Raw
 
-function job_max ($url) {
-	$a = iwr $url | % Content
-	$m = $a -match '(?ims)window.SEEK_REDUX_DATA =(.+?);\n    '
-	if (-not $m) { 'No jobs found!' ; exit }
-	$j = $Matches[1] | ConvertFrom-Json
+$match1 = [regex]::Matches($a, "(?ims)###.*?\[(.+?)\]\((.+?)\)(.+?)\!\[\]")
 
-	''
-
-	foreach ($i in $j.results.results.jobs) {
-		if ($i.salaryLabel -and $i.salaryLabel -ne 0) {
-			$i.advertiser.description
-			'    ' + $i.roleId
-			'    ' + $i.listingDateDisplay
-			'    ' + $i.salaryLabel
-			''
+foreach ($m in $match1) {
+	if ($m.Groups[2].Value -match '^/job/') {
+		$m.Groups[1].Value
+		"`t" + $base_url + $m.Groups[2].Value
+		
+		$match2 = [regex]::Match($m.Groups[3].Value, "\n\n(.+?per (month|year))")
+		if ($match2) {
+			"`t" + $match2.Groups[1].Value
 		}
+		
+		'------------------------'
 	}
-
-	''
-
-}
-
-
-function job_search ($url) {
-	$a = iwr $url | % Content
-	$m = $a -match '(?ims)window.SEEK_REDUX_DATA =(.+?);\n    '
-	if (-not $m) { 'No jobs found!' ; exit }
-	$j = $Matches[1] | ConvertFrom-Json
-
-	''
-
-	foreach ($i in $j.results.results.jobs) {
-#		if ($i.salaryLabel -and $i.salaryLabel -ne 0) {
-			$i.advertiser.description
-			'    ' + $i.roleId
-			'    ' + $i.listingDateDisplay
-			'    ' + $i.salaryLabel
-			'    https://id.jobstreet.com/job/' + $i.solMetadata.jobId
-			'    https://id.jobstreet.com/jobs?advertiserid=' + $i.advertiser.id
-			
-			''
-			
-#		}
-	}
-
-	''
-
 }
 
 
 
-#job_max 'https://id.jobstreet.com/jobs-in-information-communication-technology?salaryrange=25000000-&salarytype=monthly'
-#job_max 'https://id.jobstreet.com/jobs-in-information-communication-technology?salaryrange=40000000-&salarytype=monthly'
-#job_max 'https://id.jobstreet.com/jobs-in-information-communication-technology?salaryrange=80000000-&salarytype=monthly'
-#https://id.jobstreet.com/$_-jobs-in-information-communication-technology
-#https://id.jobstreet.com/$_-jobs
 
-$bulk_url | %{ job_search "https://id.jobstreet.com/$_-jobs-in-information-communication-technology" }
-
-
+#(Rp.+?per month)`n
 
